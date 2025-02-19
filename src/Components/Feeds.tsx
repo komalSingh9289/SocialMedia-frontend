6ab64a0713873c3f0514bb4daecb20cfe9f73155
Komal Singh <161674778+komalSingh9289@users.noreply.github.com>
2025-02-19 13:31:17 +0000

    feeds svgs added

--- src/Components/Feeds.tsx
+++ src/Components/Feeds.tsx
@@ -1,3 +1,8 @@
+import { GoPaperAirplane } from "react-icons/go";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { LiaCommentDots } from "react-icons/lia";
+import { MdMoreHoriz } from "react-icons/md";
 
 const Feeds = () => {
     const posts = [
@@ -53,21 +58,61 @@ const Feeds = () => {
             {posts.map((post) => (
                 <div key={post.id} className="w-full p-5  overflow-hidden">
                     {/* Header */}
-                    <div className="flex items-center p-4 ">
-                        <img src={post.profileImg} alt={post.user} className="w-10 h-10 rounded-full mr-3" />
-                        <span className="font-semibold text-sm">{post.user}</span>
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center p-4 ">
+                            <img src={post.profileImg} alt={post.user} className="w-10 h-10 rounded-full mr-3" />
+                            <span className="font-semibold text-sm">{post.user}</span>
+                        </div>
+                        <MdMoreHoriz className="mr-3" />
                     </div>
-
                     {/* Post Image */}
                     <img src={post.postImg} alt="Post" className="w-full h-[32rem]" />
 
                     {/* Action Buttons */}
                     <div className="flex justify-between p-4 text-xl">
                         <div className="flex space-x-4">
-                            <button>❤️</button>
-                            <button>💬</button>
-                            <button>📤</button>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="1"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="size-6 cursor-pointer"
+                            >
+                                <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1 7.8 7.8 7.8-7.8 1-1a5.5 5.5 0 0 0 0-7.8z" />
+                            </svg>
+
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="1"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="size-6 cursor-pointer"
+                            >
+                                <path d="M21 15a2 2 0 0 1-2 2H8l-4 4V5a2 2 0 0 1 2-2h13a2 2 0 0 1 2 2z" />
+                            </svg>
+
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="1"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="size-6 cursor-pointer"
+                            >
+                                <path d="M22 2 11 13" />
+                                <path d="m22 2-7 20-4-9-9-4z" />
+                            </svg>
+
                         </div>
+                        
                     </div>
 
                     {/* Likes and Caption */}
